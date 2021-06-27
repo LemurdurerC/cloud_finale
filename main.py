f9dc8c53b829e9d4ea5468843b1b8aed09f19fb4
Ubuntu <ubuntu@ip-172-31-22-208.eu-west-3.compute.internal>
2021-06-27 13:53:19 +0000

    ssl conf

--- main.py
+++ main.py
@@ -12,17 +12,18 @@ import spacy
 import os
 import cv2
 from magicPipeline import *
-
+from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
 
 app = FastAPI()
-
+#app.add_middleware(HTTPSRedirectMiddleware)
 origins = [
-    "http://localhost.tiangolo.com",
     "https://localhost.tiangolo.com",
-    "http://localhost",
-    "http://localhost:3000",
-    "http://http://localhost:8000",
-    "http://0.0.0.0"
+    "https://localhost.tiangolo.com",
+    "https://localhost",
+    "https://localhost:3000",
+    "https://localhost:8000",
+    "http://0.0.0.0",
+    "https://0.0.0.0"
 ]
 app.add_middleware(
     CORSMiddleware,
@@ -43,7 +44,10 @@ async def create_upload_file(file: UploadFile = File(...)):
         print("ok")
     except Exception as e:
         print("Lol")
-    data = magicPipeline(img)
+    try:	
+        data = magicPipeline(img)
+    except Exception as e:
+        print(e)
     sendElasticSearch(data)
 
 
@@ -53,9 +57,10 @@ def sendElasticSearch(datas):
     es = Elasticsearch()
     print("je rentre dans la fonction elastic")
     ticket = {"id": "TESTID", "company": str(datas[0]),"date": str(datas[1]), "total": str(datas[2])}
+    print(ticket)
     es.index(index="tickets", doc_type="text", body=ticket)
 
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000) #ssl_keyfile="/etc/apache2/server.key", ssl_certfile="/etc/apache2/server.crt")

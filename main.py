cec84f0035cb5ca9e5f9b5797616f7607c4ced0f
LemurdurerC <claudomirkoda@gmail.com>
2021-06-29 21:39:45 +0200

    Derniers changements liés elasticsearch + corrections éventuels bugs

--- main.py
+++ main.py
@@ -7,21 +7,26 @@ from fastapi import FastAPI, File, UploadFile
 import shutil
 from fastapi.middleware.cors import CORSMiddleware
 from elasticsearch import Elasticsearch
+from pydantic import BaseModel
 from pytesseract import pytesseract, Output
 import spacy
 import os
 import cv2
 from magicPipeline import *
 from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
+from fastapi import FastAPI, File, UploadFile,Form
+from typing import Optional
+
+from fastapi import Cookie, FastAPI
 
 app = FastAPI()
 #app.add_middleware(HTTPSRedirectMiddleware)
 origins = [
     "https://localhost.tiangolo.com",
     "https://localhost.tiangolo.com",
-    "https://localhost",
-    "https://localhost:3000",
-    "https://localhost:8000",
+    "http://localhost",
+    "http://localhost:3000",
+    "http://localhost:8000",
     "http://0.0.0.0",
     "https://0.0.0.0"
 ]
@@ -35,7 +40,7 @@ app.add_middleware(
 
 
 @app.post("/")
-async def create_upload_file(file: UploadFile = File(...)):
+async def create_upload_file(file : UploadFile = File(...),cookie: str = Form(...)):
     print("test")
     test = file.file.read()
     test2 = np.frombuffer(test,np.uint8)
@@ -44,23 +49,48 @@ async def create_upload_file(file: UploadFile = File(...)):
         print("ok")
     except Exception as e:
         print("Lol")
-    try:	
+    try:
         data = magicPipeline(img)
+        print("magicok")
     except Exception as e:
         print(e)
-    sendElasticSearch(data)
+    ticket = sendElasticSearch(data,cookie.split('=')[1])
+    ticket = str(ticket).replace("{", "")
+    ticket = str(ticket).replace("}", "")
+    return {ticket}
 
 
-def sendElasticSearch(datas):
+
+
+
+def sendElasticSearch(datas,matricule):
 
     es = Elasticsearch(HOST="http://localhost", PORT=9200)
     es = Elasticsearch()
     print("je rentre dans la fonction elastic")
-    ticket = {"id": "TESTID", "company": str(datas[0]),"date": str(datas[1]), "total": str(datas[2])}
-    print(ticket)
-    es.index(index="tickets", doc_type="text", body=ticket)
+
+    company = str(datas[0])
+    date = str(datas[1])
+    total = str(datas[2])
+
+    if company == '':
+        company= "Pas trouvé"
+    if date == '':
+        date= "Pas trouvé"
+    if total == '':
+        total= 0
+
+    total = total.replace(",",".")
+
+    ticket = {"id": matricule, "company": company,"date": date, "total": total}
+
+    try:
+        es.index(index="tickets", doc_type="text", body=ticket)
+    except Exception as e:
+        print("erreur " + str(e))
+    return ticket
 
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000) #ssl_keyfile="/etc/apache2/server.key", ssl_certfile="/etc/apache2/server.crt")
+    uvicorn.run(app, host="localhost", port=8000) #ssl_keyfile="/etc/apache2/server.key", ssl_certfile="/etc/apache2/server.crt")
